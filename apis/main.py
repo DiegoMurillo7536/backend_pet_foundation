6f0876eadc2001b7d095aff8654f4e1df2cff8f1
Diego Murillo <Diego Murillo>
2025-07-07 17:12:11 -0500

    feat: New CORS origin and fixing query

--- apis/main.py
+++ apis/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from apis.foundations import foundation_app
 from apis.donations import donation_app
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI(
     title="Pet Foundation API",
@@ -8,5 +13,16 @@ app = FastAPI(
     version="1.0.0"
 )
 
+origin = [
+    os.getenv("FRONTEND_URL"),
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origin,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(foundation_app, prefix="/api/v1")
 app.include_router(donation_app, prefix="/api/v1")
--- repositories/queries.py
+++ repositories/queries.py
@@ -2,7 +2,8 @@ queries = {
     'get_foundation_with_goals_and_actual_amount': '''
         SELECT 
             SUM(d.amount) AS total_amount,
-            gc.max_amount AS goal_amount
+            gc.max_amount AS goal_amount,
+            MAX(d.created_at) AS last_donation_date
         FROM foundations f
         LEFT JOIN goals g ON f.id = g.foundation_id
         LEFT JOIN donations d ON g.donation_id = d.id
@@ -13,8 +14,8 @@ queries = {
             f.name,
             gc.max_amount
         ORDER BY
-            d.created_at DESC
-        LIMIT 3
+            last_donation_date DESC
+        LIMIT 3;
     ''',
     'get_donors_by_foundation_id': '''
         SELECT
